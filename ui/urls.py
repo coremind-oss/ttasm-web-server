49c4719c34b0c1d1a887fae7bce04fb5128d7438
Dalibor Duric <dalibor@coremind.com.au>
2017-05-19 03:48:29 +1000

    some basic json comms

--- ui/urls.py
+++ ui/urls.py
@@ -6,4 +6,7 @@ from ui import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^inactivity_logout/$', views.showing_reverse, name='showing_reverse'),
+    
+    # DESKTOP COMMS
+    url(r'^desktop_router/$', views.desktop_router, name='desktop_router')
 ]
--- ui/views.py
+++ ui/views.py
@@ -1,7 +1,10 @@
+import json
+
 from django.contrib import messages
 from django.contrib.auth.views import logout
 from django.http.response import HttpResponse
 from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
 
 
 # Create your views here.
@@ -42,3 +45,15 @@ def showing_reverse(request):
     logout(request)
     messages.warning(request, 'You have been logged out due to inactivity')
     return redirect('/')
+
+@csrf_exempt
+def desktop_router(request):
+    print(request.POST)
+    body_json = bytes.decode(request.body)
+    parsedJSON = json.JSONDecoder().decode(body_json)
+    print(type(parsedJSON), parsedJSON)
+    some_dict = {
+        'server': 'this is a server message',
+    }
+    data = json.JSONEncoder().encode(some_dict)
+    return HttpResponse(data)
