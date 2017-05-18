886dd956fa2de88ecaf2a8136454b3ba6545623b
Shortylo85 <daneshorty@gmail>
2017-05-18 23:06:37 +0200

    added urls.py,views.py, and folder ui/template with index index page

--- ui/views.py
+++ ui/views.py
@@ -1,7 +1,32 @@
-from django.shortcuts import render
+
+
 from django.http.response import HttpResponse
+from django.shortcuts import render
+
 
 # Create your views here.
-
 def index(request):
-    return HttpResponse("this is shortys index page")
+#     return HttpResponse("this is shortys index page")
+
+#     print(request.user)
+#     print(dir(request.user))
+    
+    for item in dir(request.user):
+        print('request.user.{}'.format(item))
+
+    context_index = {'name': 'Danijel',
+                     'data':{'example':'example of data'},
+                         'some_list':[
+                             'item1',
+                             'item2',
+                             'item3',
+                             'item4',
+                             'item5',
+                             'item6',
+                             'item7',                      
+                         ]
+                    }
+    return render(request, template_name = 'index.html', context=context_index)
+
+def reverse_link(request):
+    return HttpResponse('This is reverse link built through django')
