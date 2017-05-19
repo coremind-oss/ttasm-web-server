2fad3fb065efd6fade1273a3532c468ac84c8559
Shortylo85 <daneshorty@gmail>
2017-05-19 13:15:48 +0200

    Added jquery functions for users inactivity and handling with django messages.warning. Displaying of message at custom made url named logout_page with blaze css framework

--- ui/urls.py
+++ ui/urls.py
@@ -8,4 +8,6 @@ from ui import views
 urlpatterns = [
         url(r'^$', views.index, name='index'),
         url(r'^reverse_link$', views.reverse_link,name='reverse_link'),
+#         url(r'^inactivity$',views.inactivity, name='inactivity'),
+        url(r'^logout_page/$', views.logout_page,name='logout_page')
 ]
--- ui/views.py
+++ ui/views.py
@@ -1,30 +1,32 @@
 
 
-from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import logout
 from django.contrib.auth.handlers.modwsgi import groups_for_user
+from django.http.response import HttpResponse
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
 def index(request):
 #     return HttpResponse("this is shortys index page")
-
-
-    groups = request.user.groups.all()
-    print(request.user.groups.all())
+ 
+ 
+#     groups = request.user.groups.all()
+#     print(request.user.groups.all())
+#      
+#     for group in groups:
+#         print(group)
      
-    for group in groups:
-        print(group)
-    
-    
-
-
-#     print(request.user)
-#     print(dir(request.user))
-    
-#     for item in dir(request.user):
-#         print('request.user.{}'.format(item))
-
+     
+ 
+ 
+    print(request.user)
+    print(dir(request.user))
+        
+    for item in dir(request.user):
+        print('request.user.{}'.format(item))
+ 
     context_index = {'name': 'Danijel',
                      'data':{'example':'example of data'},
                          'some_list':[
@@ -36,9 +38,20 @@ def index(request):
                              'item6',
                              'item7',                      
                          ]
-                     
+                      
                     }
     return render(request, template_name = 'index.html', context=context_index)
 
 def reverse_link(request):
     return HttpResponse('This is reverse link built through django')
+
+def inactivity(request):
+    return HttpResponse('this is inactivity')
+
+def logout_page(request):
+    logout(request)
+    messages.warning(request, 'Your session has expired')
+    return render(request, template_name = 'logout_page.html')
+  
+
+    
