9ca72945614464b890770ba38b511493b7795023
Shortylo85 <daneshorty@gmail>
2017-05-21 12:26:03 +0200

    Added Android model in TTASM project

--- data/admin.py
+++ data/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from data.models import Desktop
-
+from data.models import Desktop, Android
 # Register your models here.
 admin.site.register(Desktop)
+admin.site.register(Android)
--- data/models.py
+++ data/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 from os import name
+from django.db.models.fields.related import ForeignKey
 
 # Create your models here.
 class Desktop(models.Model):
     name = models.CharField(max_length = 255)
     datetime_registered = models.DateTimeField(auto_now_add=True)
+    
+class Android(models.Model):
+    name = models.CharField(max_length = 255)
+    datetime_registered = models.DateTimeField(auto_now_add=True)
+    desktop = models.ForeignKey('Desktop')
     
