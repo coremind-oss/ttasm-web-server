abc99493e0378867366b3a8fff60d9887a766b92
Shortylo85 <daneshorty@gmail>
2017-06-01 22:27:48 +0200

    Creating urls for signup,signin and index

--- ui/urls.py
+++ ui/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     url(r'^inactivity_logout/$', views.showing_reverse, name='showing_reverse'),
     
     # DESKTOP COMMS
-    url(r'^desktop_router/$', views.desktop_router, name='desktop_router')
+    url(r'^desktop_router/$', views.desktop_router, name='desktop_router'),
+    url(r'^sign_up/$', views.sing_up, name='sign_up'),
+    url(r'^sign_in/$', views.sign_in, name='sign_in'),
+    url(r'^index/$,', views.index, name='index')
 ]
