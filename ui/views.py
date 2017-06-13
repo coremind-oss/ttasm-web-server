2928f54caddb31224a96344b40e488adb181fbb8
Pekmen <milanpekovic89@gmail.com>
2017-06-13 15:36:20 +0200

    storing user in database

--- ui/views.py
+++ ui/views.py
@@ -1,10 +1,13 @@
 import json
 
 from django.contrib import messages
+from django.contrib.auth import authenticate
 from django.contrib.auth.views import logout
+from django.contrib.auth.models import User
 from django.http.response import HttpResponse
 from django.shortcuts import render, redirect
 from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
 
 
 # Create your views here.
@@ -41,13 +44,41 @@ def index(request):
 
     return render(request, template_name='ui/index.html', **kwargs)
 
+
+@require_http_methods(['GET', 'POST'])
 def register(request):
-    return render(request, template_name='ui/register.html')
+    if request.method == 'GET':
+        return render(request, template_name='ui/register.html')
+
+    elif request.method == 'POST':
+        form_data = request.POST
+
+        # Check if username exists
+        if User.objects.filter(username=form_data['username']).exists():
+            messages.warning(request, 'Username {} allready exists'.format(form_data['username']))
+            return(redirect('register'))
+        else:
+            username = form_data['username']
+            raw_password = form_data['password1']
+            first_name = form_data['first_name']
+            last_name = form_data['last_name']
+            email = form_data['email']
+
+            # Create user with all fields and store it in database
+            user = User.objects.create_user(username, email, raw_password)
+            user.is_active = False
+            user.first_name = first_name
+            user.last_name = last_name
+            user.save()
+
+            return(redirect('index'))
+
 
 def showing_reverse(request):
     logout(request)
     messages.warning(request, 'You have been logged out due to inactivity')
     return redirect('/')
+
 
 @csrf_exempt
 def desktop_router(request):
