11a9fbbe6630c9bb840541280b4d7f4a6026e593
Dalibor Duric <dalibor@coremind.com.au>
2017-07-05 15:11:52 +0200

    minor changes

--- data/models.py
+++ data/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.conf import settings
 from django.contrib.postgres.fields.jsonb import JSONField
-from idlelib.IOBinding import blank_re
 from django.db.models.fields.related import ForeignKey
 
-#Create your models here.
+
 class Desktop(models.Model):
     datetime_registered = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=255)
@@ -19,6 +17,6 @@ class Android(models.Model):
     desktop = models.ForeignKey(Desktop)
 
 class DailyActivity(models.Model):
-    data = JSONField(blank=True, null=True)# we're not sure if there is a limitation in JSONFiled length
+    data = JSONField(default=[])
     user = ForeignKey(User)
     
