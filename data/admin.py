9ef05c1e21a0c3c554e8b4115f6cad85344cddae
daliborduric <dalibor@coremind.com.au>
2017-08-04 14:19:55 +0200

    Update admin.py

--- data/admin.py
+++ data/admin.py
@@ -7,18 +7,18 @@ from data.models import DailyActivity
 # Register your models here.
 
 class AdminDesktop(admin.ModelAdmin):
-    list_display = ["name","uuid" ,"datetime_registered"]
+    list_display = ['name','uuid' ,'datetime_registered']
     class Meta:    
         model = Desktop
         
 class AdminAndroid(admin.ModelAdmin):
-    list_display = ["name", "desktop"]
+    list_display = ['name', 'desktop']
     class Meta:
         model = Android
 
 class AdminDailyActivity(admin.ModelAdmin):
-    list_display = ["user","base_date","data"]
-    list_filter = ["user", "base_date"]
+    list_display = ['user','base_date','data']
+    list_filter = ['user', 'base_date']
     class Meta:
         model = DailyActivity
     
