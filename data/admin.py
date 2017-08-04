d83a3680e7e48b6d21095f6d5489b3edbe3bf822
Shortylo85 <daneshorty@gmail>
2017-08-04 13:01:56 +0200

    Better display of the models in admin panel and minor changes

--- data/admin.py
+++ data/admin.py
@@ -5,6 +5,23 @@ from data.models import Android
 from data.models import DailyActivity
 
 # Register your models here.
-admin.site.register(Desktop)
-admin.site.register(Android)
-admin.site.register(DailyActivity)
+
+class AdminDesktop(admin.ModelAdmin):
+    list_display = ["name","uuid" ,"datetime_registered"]
+    class Meta:    
+        model = Desktop
+        
+class AdminAndroid(admin.ModelAdmin):
+    list_display = ["name", "desktop"]
+    class Meta:
+        model = Android
+
+class AdminDailyActivity(admin.ModelAdmin):
+    list_display = ["user","base_date","data"]
+    list_filter = ["user", "base_date"]
+    class Meta:
+        model = DailyActivity
+    
+admin.site.register(Desktop, AdminDesktop)
+admin.site.register(Android, AdminAndroid)
+admin.site.register(DailyActivity, AdminDailyActivity)
